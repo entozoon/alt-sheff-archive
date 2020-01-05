ba76133d581625552c2a47409d62c8880148d97e
Myke <entozoon@gmail.com>
2020-01-05 18:56:02 +0000

    Big fat HTTrack webwack of current (old) site

---
+++ inc/email.js
@@ -0,0 +1,10 @@
+<!--
+//<![CDATA[
+var e0 = "cont"
+var e1 = "act"
+var e2 = "alt-"
+var e3 = "sheff"
+var e4 = "org"
+document.write("<a href=" + "mail" + "to:" + e0+ e1 + "@"  + e2 + e3 + "." + e4 + ">" + e0+ e1 + "@"  + e2 + e3 + "." + e4 + "</a>");
+//]]>
+-->
